bc7b198b3f4430cb31d1d94e8690fb2ebc18d403
maheshbasnet089 <basnetmanish089@gmail.com>
2024-08-17 21:28:33 +0545

    day 4 ejs

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.send("<h1>haha world</h1>")
+    const data = {
+        name : "Manish Basnet", 
+        age : 22, 
+        location : 'itahari'
+    }
+    const nepal = {
+        continent : 'asia', 
+    }
+    res.render("home.ejs",{
+        haha : data, 
+        hehe : nepal
+    })
 })
 
 app.get('/about',(req,res)=>{
-    res.send("This is about page")
+    res.render("test/about")
 })
 
 
+app.use(express.static('public/css/'))
 
 app.listen(3000,()=>{
     console.log("project suru vayo hai tw nodejs ko")
