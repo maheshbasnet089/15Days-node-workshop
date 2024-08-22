198aad7bd264cd22f08e2070a8184ec87d61fc01
maheshbasnet089 <basnetmanish089@gmail.com>
2024-08-22 19:24:47 +0545

    learn about dbconfig,.env,make table

--- model/index.js
+++ model/index.js
@@ -1,5 +1,7 @@
 const {Sequelize,DataTypes} = require('sequelize')
 const databaseConfig = require('../config/dbConfig')
+const makeBlogTable = require('./blogModel')
+const makeUserTable = require('./userModel')
 
 
 const sequelize = new Sequelize(databaseConfig.db,databaseConfig.username,databaseConfig.password,{
@@ -27,6 +29,10 @@ const db = {}
 db.Sequelize = Sequelize 
 db.sequelize = sequelize
 
+
+
+db.blogs = makeBlogTable(sequelize,DataTypes)
+db.users = makeUserTable(sequelize,DataTypes)
 
 db.sequelize.sync({force : false}).then(()=>{
     console.log("Synced done!!")
