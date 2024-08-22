412a29ac2dd7760388ca5d9207030dc6bbb459fa
maheshbasnet089 <basnetmanish089@gmail.com>
2024-08-22 18:44:22 +0545

    ignored env file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,10 @@
+
+
 const databaseConfig = {
-    db : 'haha', 
-    username : 'root', 
-    password : '', 
-    host : '127.0.0.1', 
+    db : process.env.DB, 
+    username : process.env.USERNAME, 
+    password : process.env.PASSWORD, 
+    host : process.env.HOST, 
     port : 3306, 
     dialect : 'mysql'
 }
