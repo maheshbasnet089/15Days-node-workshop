037763f3c1027dcfe1bb78f8b9e85a3099fd31e0
maheshbasnet089 <basnetmanish089@gmail.com>
2024-09-05 19:19:32 +0545

    finish

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const express = require('express')
 const { blogs, sequelize, users } = require('./model/index')
 // const multer = require('./middleware/multerConfig').multer
 // const storage = require('./middleware/multerConfig').storage
-const {multer,storage,storage2,storage3} = require('./middleware/multerConfig')
-const upload = multer({storage:storage})
-const bcrypt = require("bcrypt")
-const { homePage, singleBlog, deleteBlog, createForm, createBlog } = require('./controller/blogController')
-const { registerUser, loginUser, renderRegister, renderLogin } = require('./controller/authController')
+
 
 
 const app = express()
+const blogRoute = require("./routes/blogRoute")
+const authRoute = require('./routes/authRoute')
 
 // app.use(express.json())
 
@@ -19,29 +17,15 @@ app.set('view engine','ejs')
 require("./model/index")
 app.use(express.urlencoded({extended : true}))
 
-app.get("/",homePage)
+// http://localhost:3000 + /
+// http://localhost:3000 + /blog/:id
+// http://localhost:3000 + /delete/:id
+// http://localhost:3000 + /create
 
-app.get("/blog/:id",singleBlog)
-
-app.get("/delete/:id",deleteBlog)
-
-
-app.get("/create",createForm)
-
-
-app.post('/create',upload.single('image') ,createBlog)
-
-
-app.get("/register",renderRegister)
-
-app.post("/register",registerUser)
-
-app.get("/login",renderLogin)
-
-app.post("/login",loginUser)
-
-
-
+app.use('',blogRoute)
+app.use('',authRoute)
+// http://localhost:3000 + register
+// http://localhost:3000 + login
 
 app.use(express.static('public/css/'))
 app.use(express.static("./storage/"))
